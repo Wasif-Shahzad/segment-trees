608ecb616411bd1dd2659d90c3e5c05f93ce283e
Wasif Shahzad <159248937+Wasif-Shahzad@users.noreply.github.com>
2024-05-02 13:48:44 +0500

    Update segment_tree_iterative.cpp

--- segmenttree.cpp
+++ segmenttree.cpp
@@ -1,45 +1,52 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+#define int long long
 
-const int MAXN = 1e5;
-int n, t[4*MAXN];
+struct SegTree{
+	const int N = 4e5+5; 		//adjust according to the problem
+	vector<int> t;;
+	void build(vector<int>& a){
+		int n = a.size();
+		t.resize(2*N, 0);
+		for(int i = 0; i < n; i++){
+			t[n+i+1] = a[i];
+		}
+		for(int i = n - 1; i > 0; i--) t[i] = t[i<<1] + t[i<<1|1];
+		// equivalent to t[i] = t[2*i] + t[2*i+1]
+		// reason is: i << j = i * (2^j) . in this case i = i and j = 1 so it becomes i * 2^1 = i * 2
+		// | is mathematically equivalent to +
+	}
 
-void build(int a[], int v, int tl, int tr) {
-    if (tl == tr) {
-        t[v] = a[tl];
-    } else {
-        int tm = (tl + tr) / 2;
-        build(a, v*2, tl, tm);
-        build(a, v*2+1, tm+1, tr);
-        t[v] = t[v*2] + t[v*2+1];
-    }
-}
+	void update(int pos, int value, int n){
+		// we will have to build it from the back starting from pos += n
+		for(t[pos += n] = value; pos > 1; pos >>= 1) t[pos>>1] = t[pos] + t[pos^1]; 
+	}
 
-int sum(int v, int tl, int tr, int l, int r) {
-    if (l > r) 
-        return 0;
-    if (l == tl && r == tr) {
-        return t[v];
-    }
-    int tm = (tl + tr) / 2;
-    return sum(v*2, tl, tm, l, min(r, tm))
-           + sum(v*2+1, tm+1, tr, max(l, tm+1), r);
-}
+	int query(int l, int r, int n){
+		// in case you can't understand, just draw a segment tree and simulate the query operation, it will become clear.
+		// blog at: https://codeforces.com/blog/entry/18051
+		int res = 0;
+		for(l += n, r += n; l < r; l >>= 1, r >>= 1){
+			if(l&1) res += t[l++];
+			if(r&1) res += t[--r];
+		}
+		return res;
+	}
 
-void update(int v, int tl, int tr, int pos, int new_val) {
-    if (tl == tr) {
-        t[v] = new_val;
-    } else {
-        int tm = (tl + tr) / 2;
-        if (pos <= tm)
-            update(v*2, tl, tm, pos, new_val);
-        else
-            update(v*2+1, tm+1, tr, pos, new_val);
-        t[v] = t[v*2] + t[v*2+1];
-    }
-}
+	void debug(int n){
+		for(int i = 0; i <= 2*n; i++) cout << t[i] << " ";
+		cout << "\n";
+	}
+};
 
-int main(){
-	cout << "hello" << endl;
+signed main(){
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for(int i = 0; i < n; i++){
+		cin >> a[i];
+	}
+	SegTree segtree;
+	segtree.build(a);
 }
